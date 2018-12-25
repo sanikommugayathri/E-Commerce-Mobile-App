8fc522b5b2ef8f3b1a04ff6dc38a665fffc04dbb
sanikommugayathri <sanikommugayathri@gmail.com>
2018-12-25 17:26:01 -0600

    Commit

--- app/src/main/java/com/example/gayat/ecommerce/ProductDetailsActivity.java
+++ app/src/main/java/com/example/gayat/ecommerce/ProductDetailsActivity.java
@@ -17,7 +17,8 @@ public class ProductDetailsActivity extends AppCompatActivity
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
 
